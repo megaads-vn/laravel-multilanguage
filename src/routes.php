72ff097189aca15660eb39f10a07ae459d90ea63
Bach Nguyen <bachnx2303@gmail.com>
2022-04-13 16:57:23 +0700

    fix: namespace on route

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,7 @@
 <?php
-Route::get('/lang-editor', ['as' => 'frontend::multilanguage::editor', 'uses' => 'Megaads\MultiLanguage\Controller\MultiLanguageController@langEditor'])->middleware('auth.lang');
-Route::post('/lang-editor', ['as' => 'frontend::multilanguage::editor', 'uses' => 'Megaads\MultiLanguage\Controller\MultiLanguageController@langEditor'])->middleware('auth.lang');
-Route::delete('/lang-editor/delete-item', ['as' => 'frontend::mutilanguage::delete::item', 'uses' => 'Megaads\MultiLanguage\Controller\MultiLanguageController@deleteItem'])->middleware('auth.lang');
-Route::get('/lang-editor/resources/{file}', ['as' => 'lang-editor::resource', 'uses' => 'Megaads\MultiLanguage\Controller\MultiLanguageController@resources']);
-Route::post('/lang-editor/add-key', ['uses' => 'Megaads\MultiLanguage\Controller\MultiLanguageController@addLanguageKey'])->middleware('auth.lang');
-Route::get('/lang-editor/download', ['uses' => 'Megaads\MultiLanguage\Controller\MultiLanguageController@downloadLanguageFile']);
+Route::get('/lang-editor', ['as' => 'frontend::multilanguage::editor', 'uses' => 'Megaads\MultiLanguage\Controllers\MultiLanguageController@langEditor'])->middleware('auth.lang');
+Route::post('/lang-editor', ['as' => 'frontend::multilanguage::editor', 'uses' => 'Megaads\MultiLanguage\Controllers\MultiLanguageController@langEditor'])->middleware('auth.lang');
+Route::delete('/lang-editor/delete-item', ['as' => 'frontend::mutilanguage::delete::item', 'uses' => 'Megaads\MultiLanguage\Controllers\MultiLanguageController@deleteItem'])->middleware('auth.lang');
+Route::get('/lang-editor/resources/{file}', ['as' => 'lang-editor::resource', 'uses' => 'Megaads\MultiLanguage\Controllers\MultiLanguageController@resources']);
+Route::post('/lang-editor/add-key', ['uses' => 'Megaads\MultiLanguage\Controllers\MultiLanguageController@addLanguageKey'])->middleware('auth.lang');
+Route::get('/lang-editor/download', ['uses' => 'Megaads\MultiLanguage\Controllers\MultiLanguageController@downloadLanguageFile']);
